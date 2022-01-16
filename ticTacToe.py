0f56b8d105fc4e458dd04d72817d69feef162e7e
Rodrigo Santos Lima <rodrigodyho@gmail.com>
2022-01-15 23:46:38 -0300

    Assignment explanation

---
+++ ticTacToe.py
@@ -0,0 +1,15 @@
+"""
+*
+Assignment: 
+W02 Prove: Developer - Solo Code Submission (Tic tac toe)
+
+1 - The game is played on a grid that is three squares by three squares.
+2 - Player one uses x's. Player two uses o's.
+3 - Players take turns putting their marks in empty squares.
+4 - The first player to get three of their marks in a row (vertically, horizontally, or diagonally) is the winner.
+5 - If all nine squares are full and neither player has three in a row, the game ends in a draw.
+
+Author = Rodrigo Lima
+*Credits = Python for absolute beginners 2019 - TIC TAC TOE project (where
+I learned how to do this project.)
+"""
