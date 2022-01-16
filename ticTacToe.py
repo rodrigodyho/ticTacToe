4c6856ff3deb1d7054a5c94df5e4873c5ccfe3c3
Rodrigo Santos Lima <rodrigodyho@gmail.com>
2022-01-15 23:52:57 -0300

    The board was created

--- ticTacToe.py
+++ ticTacToe.py
@@ -13,3 +13,32 @@ Author = Rodrigo Lima
 *Credits = Python for absolute beginners 2019 - TIC TAC TOE project (where
 I learned how to do this project.)
 """
+
+# --------- Global Variables -----------
+
+# 1- The game is played on a grid that is three squares by three squares.
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+
+# ------------- Functions ---------------
+
+# Play a game of tic tac toe
+def main():
+
+  # Show the initial game board
+  display_board()
+
+
+# Display the game board to the screen
+def display_board():
+  print("\n")
+  print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
+  print(board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
+  print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
+  print("\n")
+
+# ------------ Start Execution -------------
+# It plays the game.
+main()
